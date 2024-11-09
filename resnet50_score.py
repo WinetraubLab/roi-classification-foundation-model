644afaa88354efed24ebf634eee40642301e0f53
aye, kyi lei <98507949+kyileiaye2021@users.noreply.github.com>
2024-11-08 19:52:24 -0800

    updated resnet50_score.py and created resnet50_heatmap.py

--- resnet50_score.py
+++ resnet50_score.py
@@ -9,153 +9,56 @@ import matplotlib.colors as mcolors
 import seaborn as sns 
 import cv2
 
+'''Use this class to compute score between two patches'''
 class Resnet50Score:
     
+    '''Set up environment'''
     def __init__(self):
+        
+        # Load the pretrained Resnet50 model
         self.model = models.resnet50(pretrained=True)
-        self.model = nn.Sequential(*list(self.model.children())[:-1]) #  Remove the final classification layer
+        
+        # Remove the final classification layer
+        self.model = nn.Sequential(*list(self.model.children())[:-1]) 
+        
+        # Set the model to evaluation mode
         self.model.eval()
         
-        self.preprocess = transforms.Compose([
+        # Define image transform
+        self.transform = transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229,0.224, 0.225]),
         ])
-            
-    def extract_embeddings(self, image):
-        '''
-        Extracts the embedding from the image using the ResNet50 model.
-        '''
-        # load and preprocess the image
-        img_tensor = self.preprocess(image).unsqueeze(0)
         
-        # extract embeddings
+    """ This function computes embedding of a patch (PIL.Image) """       
+    def compute_patch_embedding(self, patch, patch_size=256):
+        
+        # Check patch sizes
+        width, height = patch.size
+        if width != patch_size or height != patch_size:
+            raise ValueError(f"patch_a and patch_b should be of size {patch_size}")
+        
+        # load and transform the patch
+        patch_t = self.transform(patch).unsqueeze(0)
+        
+        # Compute feature embeddings
         with torch.no_grad():
-            embeddings = self.model(img_tensor)
-        return embeddings
+            patch_emb = self.model(patch_t)
+        
+        # convert feature embeddings to 1D NumPy arrays (in cosine similarity, each embedding needed to be 1D vec)
+        patch_emb = patch_emb.cpu().numpy().flatten()
+        return patch_emb
     
+    def compute_similarity_between_embeddings(self, patch_a_emb, patch_b_emb):
+        cosine_similarity = np.dot(patch_a_emb, patch_b_emb) / (np.linalg.norm(patch_a_emb) * np.linalg.norm(patch_b_emb))
+        return cosine_similarity
     
-    def extract_patches(self, image_patch, patch_folder, embedding_folder, patch_size = 256):
-        '''
-        Extracts image patches from the Test images.
-        '''
-        os.makedirs(patch_folder, exist_ok=True)
-        os.makedirs(embedding_folder, exist_ok=True)
-        
-        # open the large test image
-        image = Image.open(image_patch).convert('RGB')
-        
-        # calculate the num of patches in each dimension
-        width, height = image.size
-        num_patches_x = width // patch_size
-        num_patches_y = height // patch_size
-        
-        # Extract patches
-        for i in range(num_patches_y): # height 
-            for j in range(num_patches_x): # width
-                
-                # define the box to cut out
-                box = (j * patch_size, i * patch_size, (j + 1) * patch_size, (i + 1) * patch_size)
-                patch = image.crop(box)
-                
-                patch_path = os.path.join(patch_folder, f'patch_{i}_{j}.png') # folder where the patches will be saved
-                patch.save(patch_path) # save patches in the folder 
-                
-                # extract features
-                embeddings = self.extract_embeddings(patch)
-                np.save(os.path.join(embedding_folder, f"patch_{i}_{j}.npy"), embeddings) # saving patch embeddings
-                
-
-    def calculate_cosine_sim(self, ref_embedding_path, embedding_folder):
-        '''
-        Calculate the cosine similarity between the reference embeddings and the embeddings of the patches.
-        '''
-        ref_embeddings = np.load(ref_embedding_path).flatten() # load the ref img features
-        
-        # need to calculate the num of row and columns for creating heatmap grid
-        max_rows = 0
-        max_cols = 0
-        
-        # This is for counting the num of patches and for creating grid for simiilarity heatmap
-        for filename in os.listdir(embedding_folder):
-            if filename.endswith(".npy"):
-                parts = filename.split('_')
-                row = int(parts[-2]) # patch row index
-                col = int(parts[-1].split('.')[0]) # patch column index
-                
-                max_rows = max(max_rows, row)
-                max_cols = max(max_cols, col)
-                
-        similarity_grid = np.zeros((max_rows + 1, max_cols + 1), dtype=float)  # +1 because indices start at 0
-
-        # This is for loading the similarity grid with embeddings
-        for filename in os.listdir(embedding_folder):
-            if filename.endswith(".npy"):
-                
-                parts = filename.split('_')
-                row = int(parts[-2])
-                col = int(parts[-1].split('.')[0])  # Patch column index
-                
-                # load and flatten test image features
-                test_embeddings = np.load(os.path.join(embedding_folder, filename)).flatten()
-                
-                # calculate cosine similarity
-                cosine_similarity = np.dot(ref_embeddings, test_embeddings) / (np.linalg.norm(ref_embeddings) * np.linalg.norm(test_embeddings))
-                
-                similarity_grid[row, col] = cosine_similarity
-        return similarity_grid
+    """ This function computes the score between two patches.
+    Both patches should be a PIL.Image class """
+    def compute_similarity_between_patches(self, patch_a, patch_b, patch_size=256):
+        patch_a_emb = self.compute_patch_embedding(patch_a, patch_size)
+        patch_b_emb = self.compute_patch_embedding(patch_b, patch_size)
+        return self.compute_similarity_between_embeddings(patch_a_emb, patch_b_emb)
     
-    
-    def heatmap(self, ref_embedding_path, embedding_folder, heatmap_file_path, overlay_heatmap_path):
-        '''
-        Create a similarity heatmap based on the cosine similarity in each patch
-        '''
-        
-        similarity_heatmap_grid = self.calculate_cosine_sim(ref_embedding_path, embedding_folder)
-        heatmap = np.array(similarity_heatmap_grid)
-        colors = [(0.0, "darkgreen"),
-                  (0.65, "green"),
-                  (0.72, "red"),
-                  (1.0, "darkred")]
-        
-        cmap = mcolors.LinearSegmentedColormap.from_list("custom_cmap", colors)
-        
-       
-        # showing and saving similarity heatmap
-        plt.figure(figsize=(10,8))
-        sns.heatmap(heatmap, cmap="RdYlGn_r", annot=True, fmt=".2f", cbar=True)
-        plt.title('RCM Cosine Similarity Heatmap with ResNet50')
-        plt.savefig(heatmap_file_path, format='png', dpi=300, bbox_inches='tight')
-        plt.show()
-        print("Changed the heatmap!!")
-        
-        # for the purpose of overlaying heatmap onto the test image
-        plt.figure(figsize=(10,8))
-        sns.heatmap(heatmap, cmap="RdYlGn_r", annot=True, fmt=".2f", cbar=False)
-        plt.xticks([])  # Remove x-axis labels
-        plt.yticks([])  # Remove y-axis labels
-        plt.savefig(overlay_heatmap_path, format='png', dpi=300, bbox_inches='tight')
-        plt.close()  # Close the plot to free up memory 
-        
-        
-    def overlay_heatmap(self, overlay_heatmap_path, original_image):
-        '''
-        Overlay the heatmap onto the original image
-        '''
-        
-        original_image = cv2.imread(original_image)
-        heatmap = cv2.imread(overlay_heatmap_path)
-        
-        # Resize the heatmap if necessary to match the original image size
-        heatmap = cv2.resize(heatmap, (original_image.shape[1], original_image.shape[0]))
-        
-        overlay = original_image.copy()
-        alpha = 0.5 # Adjust transparency level (0: fully transparent, 1: fully opaque)
-        overlay = cv2.addWeighted(original_image, 1 - alpha, heatmap, alpha, 0)
-        
-        plt.title('Overlayed RCM Heatmap From ResNet50')
-        plt.imshow(cv2.cvtColor(overlay, cv2.COLOR_BGR2RGB))
-        plt.axis('off')
-        plt.savefig(overlay_heatmap_path, format='png', dpi=300, bbox_inches='tight')
-        plt.show()
